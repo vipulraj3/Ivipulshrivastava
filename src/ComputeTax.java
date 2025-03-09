c729d3fda94873048f4bfd18d5350d03ad2daea5
vipul raj <rajvipul74@gmail.com>
2025-03-09 18:15:56 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/ComputeTax.java
@@ -0,0 +1,86 @@
+import java.util.*;
+// For each filing status there are six tax rates. Each rate is applied to a certain amount of
+// tax-able income. For example, of a taxable income of $400,000 for single filers, $8,350 is taxed
+// at 10%, (33,950- 8,350) at 15%, (82,250- 33,950) at 25%, (171,550- 82,250) at 28%,
+// (372,950- 171,550) at 33%, and (400,000- 372,950) at 35%.
+
+public class ComputeTax {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("(0-single filer, 1-married jointly or " +
+                "qualifying widow(er), 2-married separately, 3-head of " +
+                "household) Enter the filing status: ");
+        int status = input.nextInt();
+
+        System.out.print("Enter the taxable income: ");
+        double income = input.nextDouble();
+
+        double tax = 0;
+
+        if(status == 0){ // single filer
+            if (income <= 8350)
+                tax = income * 0.10;
+            else if(income <= 33950)
+                tax = 8350 * 10 + (income - 8350) * 0.15;
+            else if (income <= 82250)
+                tax = 8350 * 10 + (33950 - 8350) + (income - 33950) * 0.25;
+            else if (income <= 171550)
+                tax = 8350 * 10 + (33950 - 8350) + (82250 - 33950) * 0.25 + (income - 82250) * 0.28;
+            else if (income <= 372950)
+                tax = 8350 * 10 + (33950 - 8350) + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (income - 171550) * 0.33;
+            else
+                tax = 8350 * 10 + (33950 - 8350) + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (372950 - 171550) * 0.33 + (income - 372950) * 0.35;
+        }
+        else if (status == 1){ // Married filing jointly or qualifying widow(er)
+            if (income <= 16700)
+                tax = income * 0.10;
+            else if (income <= 67900)
+                tax = 16700 * 0.10 + (income - 16700) * 0.15;
+            else if (income <= 137050)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 67900) * 0.25;
+            else if (income <= 208850)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (income - 137050) * 0.28;
+            else if (income <= 372950)
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137050) * 0.28 + (income - 208850) * 0.33;
+            else
+                tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137050) * 0.28 + (372950 - 208850) * 0.33 + (income - 372950) * 0.35;
+        }
+        else if(status == 2){ // Married filing separately
+            if (income <= 8350)
+                tax = income * 0.10;
+            else if (income <= 33950)
+                tax = 8350 * 0.10 + (income - 8350) * 0.15;
+            else if (income <= 68525)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25;
+            else if (income <= 104425)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (income - 68525) * 0.28;
+            else if (income <= 186475)
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (104425 - 68525) * 0.28 + (income - 104425) * 0.33;
+            else
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (104425 - 68525) * 0.28 + (186475 - 104425) * 0.33 + (income - 186475) * 0.35;
+        }
+        else if(status == 3){ // Head of household
+            if (income <= 11950)
+                tax = income * 0.10;
+            else if (income <= 45500)
+                tax = 11950 * 0.10 + (income - 11950) * 0.15;
+            else if (income <= 117450)
+                tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (income - 45500) * 0.25;
+            else if (income <= 190200)
+                tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (income - 117450) * 0.28;
+            else if (income <= 372950)
+                tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (190200 - 117450) * 0.28 + (income - 190200) * 0.33;
+            else
+                tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (190200 - 117450) * 0.28 + (372950 - 190200) * 0.33 + (income - 372950) * 0.35;
+        }
+        else {
+            System.out.println("Error: Invalid status");
+            System.exit(1);
+            // system.exit(status) is defined in the system class. Invoking this method terminates the program.
+            // The status 0 indicates that the program is terminated normally. A nonZero status code indicates abnormal termination.
+
+        }
+        System.out.println( "Tax is: " + tax);
+    }
+}
