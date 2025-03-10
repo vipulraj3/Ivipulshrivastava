558daedab8f8a6438a49b4cc65be57559e71a2dd
vipul raj <rajvipul74@gmail.com>
2025-03-10 21:12:40 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/Test12.java
@@ -0,0 +1,11 @@
+public class Test12 {
+    public static void main(String[] args){
+
+
+        int randomValue = Math.random() < 0.5 ? -1 : 1;
+        // Math.random variable generates a random value between 0.0 and 1.0.
+
+        System.out.print("Random value: " + randomValue);
+
+    }
+}
