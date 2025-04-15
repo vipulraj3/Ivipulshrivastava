89bab00c08e783975c4f90947ac8514dac8a1493
vipul raj <rajvipul74@gmail.com>
2025-04-15 20:35:45 +0530

    Merge remote-tracking branch 'origin/main'

--- src/RandomMonth.java
+++ src/RandomMonth.java
@@ -1,7 +1,23 @@
-import java.util.*;
-
 public class RandomMonth {
     public static void main(String[] args){
+        int ch = (int)(Math.random() * 12) + 1;
 
+        String monthname = "";
+
+        switch (ch) {
+            case 1: monthname = "January" ; break;
+            case 2: monthname = "february" ; break;
+            case 3: monthname = "march" ; break;
+            case 4: monthname = "april" ; break;
+            case 5: monthname = "may" ; break;
+            case 6: monthname = "june" ; break;
+            case 7: monthname = "july" ; break;
+            case 8: monthname = "august" ; break;
+            case 9: monthname = "september" ; break;
+            case 10: monthname = "october" ; break;
+            case 11: monthname = "november" ; break;
+            case 12: monthname = "december" ; break;
+        }
+        System.out.println("The randomly selected month is: " + monthname);
     }
 }
---
+++ src/RandomMonth1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class RandomMonth1 {
+    public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+
+
+    }
+}
